06d790fac0272a67e8419368a9f1edfa3cc4f4c2
Md.Johirule <johirulislam574206@gmail.comm>
2022-10-25 15:53:47 +0600

    Data update

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
+// Data loge in json file
+//cors
+app.use(cors());
+
+const courses = require('./Data/wb-technology-data.json')
 
 
 
 app.get('/', (req, res) => {
     res.send("Server is Runing")
+})
+//all course 
+app.get('/courses', (req, res) =>{
+    res.send(courses)
 })
 
 app.listen(port, () => {
