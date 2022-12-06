47288d8229330a300c21b831ff8da385b235f01d
Md.Johirule <johirulislam574206@gmail.comm>
2022-12-06 23:21:57 +0600

    server site set mongoDb database, get route setup, etc

--- index.js
+++ index.js
@@ -3,12 +3,53 @@ const app = express();
 // server run 5000 port
 const port = process.env.PORT || 5000;
 const cors = require('cors')
-// Data loge in json file
-//cors
+
+require('dotenv').config()
+
+//middle ware
 app.use(cors());
+app.use(express.json())
+
+// MongoDb connected
 
 
-const coureDetails = require('./Data/courseDetails.json')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@rasel-01.uhpxwkk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const SkillCollection = client.db('Portfolio').collection('Skills');
+
+        app.get('/skills', async (req, res) => {
+            const query = {};
+            const result = await SkillCollection.find(query).toArray();
+            res.send(result);
+
+        })
+        //coursess Id 
+        app.get('/skills/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { id: id }
+            const result = await SkillCollection.findOne(query)
+            console.log("first", result)
+            res.send(result);
+
+        })
+
+
+
+    } catch (err) {
+        console.log(err);
+    }
+
+
+}
+
+run().catch(err => console.log(err))
+
+//mongoDb end
+
 
 // server started our nodemon index.js
 app.get('/', (req, res) => {
@@ -17,20 +58,16 @@ app.get('/', (req, res) => {
 
 
 //coursess Id 
-app.get('/course/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedCourse = coureDetails.find(c => c._id === id)
-    res.send(selectedCourse);
+// app.get('/course/:id', (req, res) => {
+//     const id = req.params.id;
+//     const selectedCourse = coureDetails.find(c => c._id === id)
+//     res.send(selectedCourse);
 
-})
+// })
+
 
 //clg show web-technology-is rouning
 app.listen(port, () => {
     console.log("Web-technology- is Runing", port)
 })
-
-app.get('/course', (req, res) => {
-    res.send(coureDetails);
-})
-
 //that is ?
