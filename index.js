16e5bd387163e80d9fd3e5f044b7724f3ff7db9b
johirul-islam-6 <johirulislam574206@gmail.com>
2023-08-10 18:56:06 +0600

    review add

--- index.js
+++ index.js
@@ -19,8 +19,70 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
+        const cetagoryCollection = client.db('Portfolio').collection('Cetagorys');
         const SkillCollection = client.db('Portfolio').collection('Skills');
+        const projectCollection = client.db('Portfolio').collection('project_lisht');
+        const reviewCollection = client.db('Portfolio').collection('review');
 
+        // post review
+ app.post('/review', async (req, res) => {
+  const data = req.body;
+  try {
+    // Insert the review data into the MongoDB collection
+    const result = await reviewCollection.insertOne(data);
+
+    res.status(201).json({ message: 'Review added successfully', reviewId: result.insertedId });
+  } catch (error) {
+    console.error('Error inserting review:', error);
+    res.status(500).json({ message: 'An error occurred while adding the review' });
+  }
+});
+
+ app.get('/reviews', async (req, res) => {
+            
+            const result = await reviewCollection.find({ }).toArray();
+            res.send(result);
+
+        })
+
+
+        app.get('/cetegors', async (req, res) => {
+            const query = {};
+            const result = await cetagoryCollection.find(query).toArray();
+            res.send(result);
+
+        })
+        app.get('/development/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { development: id };
+            const result = await SkillCollection.find(query).toArray();
+            res.send(result);
+
+        })
+        // ======PROJECT===========
+        app.get('/simple', async (req, res) => {
+            const query = {};
+            const result = await projectCollection.find(query).toArray();
+            res.send(result);
+
+        })
+        app.get('/simple-project/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { id: id };
+            const result = await projectCollection.find(query).toArray();
+            res.send(result);
+
+        })
+        //all skill select one skill details
+        app.get('/details/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { id: id };
+            console.log(query);
+            const result = await SkillCollection.find(query).toArray();
+            res.send(result);
+
+        })
+        //skill base data lode in data base
         app.get('/skills', async (req, res) => {
             const query = {};
             const result = await SkillCollection.find(query).toArray();
@@ -32,7 +94,7 @@ async function run() {
             const id = req.params.id;
             const query = { id: id }
             const result = await SkillCollection.findOne(query)
-            console.log("first", result)
+            // console.log("first", result)
             res.send(result);
 
         })
